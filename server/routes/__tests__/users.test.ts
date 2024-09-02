8cc90273dac06c5690c32e359fb2a70171521ae3
Hyunsoo An <hankjudolove@gmail.com>
2024-09-03 11:51:54 +1200

    completed test for the route and db

---
+++ server/routes/__tests__/users.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, expect, beforeAll, beforeEach, afterAll } from 'vitest'
+
+import connection from '../../db/connection.ts'
+import server from '../../server.ts'
+import request from 'supertest'
+
+beforeAll(async () => {
+  await connection.migrate.latest()
+})
+
+beforeEach(async () => {
+  await connection.seed.run()
+})
+
+afterAll(async () => {
+  await connection.destroy()
+})
+
+// Getting allUserProtiles
+describe('getting all user profiles', () => {
+  it('gets the user profiles', async () => {
+    const res = await request(server).get('/api/v1/users/')
+    expect(res.status).toBe(200)
+  })
+})
