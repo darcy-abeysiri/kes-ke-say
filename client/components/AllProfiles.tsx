43a8ddbca678558bd5a2ae97764568448920a51e
Hyunsoo An <hankjudolove@gmail.com>
2024-09-03 11:37:12 +1200

    completed component

--- client/components/AllProfiles.tsx
+++ client/components/AllProfiles.tsx
@@ -1,24 +1,42 @@
 import { useQuery } from '@tanstack/react-query'
 import { getUsers } from '../apis/userApi'
+import { User } from '../../models/user.ts'
 
 function Users() {
-  const { data, isPending, isError, error } = useQuery({
+  const { data, isLoading, isError, error } = useQuery<User[]>({
     queryKey: ['getUsers'],
     queryFn: () => getUsers(),
   })
 
-  if (isPending) {
+  if (isLoading) {
     return <p>Loading...</p>
   }
 
   if (isError) {
-    return <p>Error...</p>
+    return error
   }
 
   return (
-    <>
-      <h1>All Profiles...</h1>
-    </>
+    <div className="grid grid-cols-3 gap-4">
+      {data.map((user) => (
+        <div
+          key={user.id}
+          className="flex flex-col items-center border border-solid rounded-lg bg-slate-300"
+        >
+          <img
+            src={`../../public/images/avatars/${user.image}`}
+            alt="user profile"
+            className="w-64 mt-5 mb-2.5"
+          ></img>
+          <div>
+            <h3>
+              <strong>{user.username}</strong>
+            </h3>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
+
 export default Users
