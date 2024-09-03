0c31bba853e43acad435bfceb90f5fb19b95ce91
Hayden Flintoft <hflintoft@gmail.com>
2024-09-04 11:30:25 +1200

    Complete Routes and API bad route tests.

--- server/routes/__tests__/posts.test.ts
+++ server/routes/__tests__/posts.test.ts
@@ -2,7 +2,6 @@ import request from 'supertest'
 import { expect, test, beforeAll, afterAll, beforeEach } from 'vitest'
 import server from '../../server.ts'
 import db from '../../db/connection'
-// import { Request, Response, NextFunction } from 'express'
 
 beforeAll(async () => {
   await db.migrate.latest()
@@ -15,34 +14,6 @@ beforeEach(async () => {
 afterAll(async () => {
   await db.destroy()
 })
-
-// describe('GET ap1/v1/posts', () => {
-//   it('should show the posts', async () => {
-//     vi.mocked(fruitsDb.getFruits).mockResolvedValue(mockFruits)
-
-//     const res = await request(server).get('/api/v1/fruits')
-
-//     expect(res.statusCode).toBe(200)
-//     expect(res.body).toMatchInlineSnapshot(`
-//       {
-//         "fruits": [
-//           {
-//             "addedByUser": "auth0|123",
-//             "averageGramsEach": 120,
-//             "id": 1,
-//             "name": "Banana",
-//           },
-//           {
-//             "addedByUser": "auth0|456",
-//             "averageGramsEach": 195,
-//             "id": 2,
-//             "name": "Apple",
-//           },
-//         ],
-//       }
-//     `)
-//   })
-// })
 
 test('GET /api/v1/posts should return an array of posts', async () => {
   const response = await request(server).get('/api/v1/posts')
@@ -69,4 +40,3 @@ test('GET /api/v1/posts should return an empty array when no posts are available
   expect(response.status).toBe(200)
   expect(response.body).toHaveLength(0)
 })
-
