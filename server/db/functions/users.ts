503c0b68ccc0464ce67166ead110b9f4de53ec2f
Hyunsoo An <hankjudolove@gmail.com>
2024-09-03 15:52:59 +1200

    working on backend single

--- server/db/functions/users.ts
+++ server/db/functions/users.ts
@@ -14,3 +14,32 @@ export function getAllUsers(): Promise<User[]> {
     'image',
   )
 }
+
+// Getting each user id
+export function getUserById(id: number): Promise<User> {
+  return db('users')
+    .where({ id })
+    .first()
+    .select(
+      'id',
+      'auth0_id as auth0Id',
+      'username',
+      'full_name as fullName',
+      'location',
+      'image',
+    )
+}
+
+// Updating user info
+export function updateUser(
+  id: number,
+  updates: {
+    auth0_id?: string
+    username?: string
+    full_name?: string
+    location?: string
+    image?: string
+  },
+) {
+  return db('users').where({ id }).update(updates)
+}
--- server/routes/users.ts
+++ server/routes/users.ts
@@ -14,4 +14,35 @@ router.get('/', async (req, res) => {
   }
 })
 
+// GET 'api/v1/users/:id'
+router.get('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  try {
+    const user = await db.getUserById(id)
+    res.json(user)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
+//Patch UserProfile
+router.patch('/:id', async (req, res) => {
+  const id = Number(req.params.id)
+  const { auth0_id, username, full_name, location, image } = req.body
+  try {
+    const user = await db.updateUser(id, {
+      auth0_id,
+      username,
+      full_name,
+      location,
+      image,
+    })
+    res.json(user)
+  } catch (error) {
+    console.error(`Database error: ${error}`)
+    res.sendStatus(500)
+  }
+})
+
 export default router
