dde93b2781a1830ce11bd9edbf177c4b356296be
Karl Bloomfield <krbloomfield@gmail.com>
2024-09-03 15:12:32 +1200

    AllGroups component

--- client/components/AllGroups.tsx
+++ client/components/AllGroups.tsx
@@ -20,20 +20,27 @@ export default function AllGroups() {
 
   return (
     <>
-      <button type="submit" className="button-primary">
-        Add a group
-      </button>
-      {data?.map((groups) => (
-        <div key={groups.id}>
-          <div>
-            <img
-              src={`../../Public/images/icons/${groups.image}`}
-              alt={`${groups.image}`}
-            />
-            <p>{groups.name}</p>
+      <div className="clearfix">
+        <button
+          type="submit"
+          className="float-right bg-[#333333] text-white fond-bold mr-5 mb-10 mt-10 py-2 px-4 rounded w-40"
+        >
+          Add a group
+        </button>
+      </div>
+      <div className="grid grid-cols-3 gap-4 place-content-center">
+        {data?.map((groups) => (
+          <div key={groups.id} className="w-40 ml-20">
+            <div>
+              <img
+                src={`../../Public/images/icons/${groups.image}`}
+                alt={`${groups.image}`}
+              />
+              <p className="text-center">{groups.name}</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </>
   )
 }
