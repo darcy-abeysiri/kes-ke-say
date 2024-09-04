e15ad813272cfb28babf3a4b96ccb02e61729023
Hayden Flintoft <hflintoft@gmail.com>
2024-09-05 11:39:28 +1200

    Create post comp

--- client/components/Post.tsx
+++ client/components/Post.tsx
@@ -23,38 +23,48 @@ export default function Post() {
   }
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="w-1/2">
-        <div>
+    <div className="flex flex-col items-center m-10 ">
+      <div className="w-1/2 shadow-md shadow-gray-600 p-10 rounded-md">
+        <div className="size-full">
           <img
             src={post.image}
             alt=""
             className="
-        rounded-3xl 
-        shadow-lg
+        rounded-3xl
+        drop-shadow-md
         shadow-gray-600
-        m-10
+        mb-10
                 "
           />
         </div>
 
         {/* <h2>{post.username}</h2> */}
-        <div>
-          {/* <img src={`images/avatars/${post.userImage}`} alt={post.userImage} className=""/> */}
+
+        <div className="text-lg">{post.body}</div>
+        <div className="text-slate-600 my-5 italic flex flex-col-auto mt-20 ">
+          <div className="size-[3rem] mr-5">
+            {post.image && (
+              <img
+                src={`/images/avatars/${post.userImage}`}
+                alt="User Avatar"
+              />
+            )}
+          </div>
+          <div className="">
+            <p>
+              Posted by{' '}
+              <Link to={`/profiles/${post.username}`}>
+                <strong>{post.username}</strong>
+              </Link>{' '}
+              on{' '}
+              {`${new Date(post.createdAt).toLocaleString('en-NZ', { hour12: false })}`}
+            </p>
+          </div>
         </div>
-        <div className="tre">{post.body}</div>
-        <div className="text-slate-600 my-5 italic text-right">
-          <p>
-            Posted by{' '}
-            <Link to={`/profiles/${post.username}`}>
-              <strong>{post.username}</strong>
-            </Link>{' '}
-            on{' '}
-            {`${new Date(post.createdAt).toLocaleString('en-NZ', { hour12: false })}`}
-          </p>
+        <div className="m-10">
+          <Voting />
         </div>
       </div>
-      <div className='m-10'><Voting /></div>
     </div>
   )
 }
