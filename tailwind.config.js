443905dc132ccab764054d3a830ab1952e741ccf
Jeremy Webster <jeremy.webster106@gmail.com>
2024-09-04 13:40:26 +1200

    add custom colours to tailwind.config.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
   content: ['./index.html', './client/**/*.{js,ts,jsx,tsx}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "frenchWhite": "#dcd8d6",
+        "frenchBlue": "#364da1",
+        "frenchRed": "#4f0404",
+        "frenchCharcoal": "#333333",
+      }
+    },
   },
   plugins: [],
 }
