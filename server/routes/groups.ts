b8eaaa566e9ba0ff0c8fb9e8304a30d26e8f91fe
Karl Bloomfield <krbloomfield@gmail.com>
2024-09-04 15:15:58 +1200

    Fix near fatal test failures

--- server/routes/groups.ts
+++ server/routes/groups.ts
@@ -1,9 +1,16 @@
 import express from 'express'
+import * as db from '../db/functions/groups'
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).send('Hello from the groups route!')
+router.get('/', async (req, res) => {
+  try {
+    const groups = await db.getAllGroups()
+    res.json(groups).status(200)
+  } catch (error) {
+    console.error(`Could not find all groups: ${error}`)
+    res.sendStatus(500)
+  }
 })
 
 export default router
