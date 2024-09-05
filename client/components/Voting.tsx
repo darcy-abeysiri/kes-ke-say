c5653bd8257aef7b34c37cc05954de0f5630ba7a
Jeremy Webster <jeremy.webster106@gmail.com>
2024-09-05 14:16:59 +1200

    add voting button animations

--- client/components/Voting.tsx
+++ client/components/Voting.tsx
@@ -1,37 +1,41 @@
 export default function Voting() {
   return (
     <div className=" flex flex-col-auto gap-5">
-      <img src="/images/icons/car-darkgray.png" alt="Car" className="size-12" />
       <img
         src="/images/icons/cheese-darkgray.png"
         alt="Car"
-        className="size-12"
-      />
-      <img
-        src="/images/icons/fries-darkgray.png"
-        alt="Car"
-        className="size-12"
-      />
+        className="size-12 opacity-50 hover:opacity-100 hover:scale-150 transition"
+        />
       <img
         src="/images/icons/glasses-darkgray.png"
         alt="Car"
-        className="size-12"
-      />
+        className="size-12 opacity-50 hover:opacity-100 transition hover:animate-spin"
+        />
       <img
         src="/images/icons/icon-darkgray.png"
         alt="Car"
-        className="size-12"
-      />
+        className="size-12 opacity-50 hover:opacity-100 transition hover:animate-bounce"
+        />
       <img
         src="/images/icons/sunrise-darkgray.png"
         alt="Car"
+        className="size-12 opacity-50 hover:opacity-100 transition hover:animate-ping"
+        />
+        {/* <img 
+          src="/images/icons/car-darkgray.png" 
+          alt="Car" 
+          className="size-12" 
+          /> */}
+      {/* <img
+        src="/images/icons/fries-darkgray.png"
+        alt="Car"
         className="size-12"
-      />
-      <img
+      /> */}
+      {/* <img
         src="/images/icons/taco-darkgray.png"
         alt="Car"
         className="size-12"
-      />
+      /> */}
     </div>
   )
 }
